fc84e719f89793ee1bc834789c8845b797a51ff4
LifeIsTech <funkinaoki@gmail.com>
2021-02-06 13:57:08 +0900

    Initial Commit

---
+++ Techpod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techpod
+//
+//  Created by 八幡尚希 on 2021/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
